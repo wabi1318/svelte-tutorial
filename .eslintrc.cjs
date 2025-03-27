5f6bfcfbf14d604427aab20a5750a1a33576802c
may <bankouzhenyou8@gmail.com>
2025-03-27 14:21:21 +0900

    eslint, prettier追加

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:svelte/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    extraFileExtensions: [".svelte"],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+    },
+  ],
+  rules: {
+    // カスタムルールをここに追加
+  },
+};
