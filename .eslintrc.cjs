7404b5f1af06d4e6a922beaebd3b9787af097788
may <bankouzhenyou8@gmail.com>
2025-04-07 10:27:11 +0900

    サイト作成

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,15 @@
 module.exports = {
   root: true,
-  extends: ["eslint:recommended", "plugin:svelte/recommended", "prettier"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2020,
-    extraFileExtensions: [".svelte"],
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true,
-  },
-  overrides: [
-    {
-      files: ["*.svelte"],
-      parser: "svelte-eslint-parser",
-    },
-  ],
-  rules: {
-    // カスタムルールをここに追加
-  },
+  extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
+  // env: {
+  //   browser: true,
+  //   es2017: true,
+  //   node: true,
+  // },
+  // overrides: [
+  //   {
+  //     files: ['*.svelte'],
+  //     parser: 'svelte-eslint-parser',
+  //   },
+  // ],
 };
--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,44 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let product = {
+    id: 'svelte-book',
+    name: 'Svelte book',
+    price: 3500,
+    images: [
+      'https://github.com/svelte-book/sample-app/raw/main/static/react-book-1.png',
+      'https://github.com/svelte-book/sample-app/raw/main/static/react-book-2.png',
+      'https://github.com/svelte-book/sample-app/raw/main/static/react-book-3.png',
+    ],
+  };
+
+  let relatedProducts = [
+    { id: 'react-book', name: 'React book', price: 3500 },
+    { id: 'vue-book', name: 'Vue book', price: 3500 },
+    { id: 'angular-book', name: 'Angular book', price: 3500 },
+  ];
+
+  let cart = [];
+
+  const addToCart = (productId) => {
+    cart = [...cart, productId];
+    // cart.push(productId)
+  };
+
+  let sliderCenterIndex = 0;
+  let sliderLeftIndex = product.images.length - 1;
+  let sliderRightIndex = 1;
+
+  function sliderMoveLeft() {
+    sliderCenterIndex = (sliderCenterIndex - 1 + product.images.length) % product.images.length;
+    sliderLeftIndex = (sliderCenterIndex - 1 + product.images.length) % product.images.length;
+    sliderRightIndex = (sliderCenterIndex + 1) % product.images.length;
+  }
+
+  function sliderMoveRight() {
+    sliderCenterIndex = (sliderCenterIndex + 1) % product.images.length;
+    sliderLeftIndex = (sliderCenterIndex - 1 + product.images.length) % product.images.length;
+    sliderRightIndex = (sliderCenterIndex + 1) % product.images.length;
+  }
 </script>
-
-<header class="header">
-  <a class="header-title" href="/">Svelte EC</a>
-  <nav>
-    <ul class="header-links">
-      <li>ようこそゲストさん</li>
-      <li>
-        <a href="/cart">カート (0)</a>
-      </li>
-    </ul>
-  </nav>
-</header>
-
-<article class="product">
-  <div class="product-main">
-    <div class="image-container">
-      <img src="https://github.com/svelte-book/sample-app/raw/main/static/react-book-1.png" alt="『Svelte Guide』表紙" />
-    </div>
-  </div>
-
-  <div>
-    <h2>Svelte Guide</h2>
-    <dl>
-      <dt>価格</dt>
-      <dd>3500円</dd>
-    </dl>
-    <div>
-      <button>カートに入れる</button>
-    </div>
-  </div>
-
-  <footer>
-    <h3>関連商品</h3>
-    <ul>
-      <li>
-        <a href="/products/react-book">React book</a>
-        - 3500円
-      </li>
-      <li>
-        <a href="/products/vue-book">Vue book</a>
-        - 3500円
-      </li>
-      <li>
-        <a href="/products/angular-book">Angular book</a>
-        - 3500円
-      </li>
-    </ul>
-  </footer>
-</article>
 
 <style>
   :global(body) {
@@ -72,5 +59,143 @@
     height: 50px;
   }
 
+  .header-links {
+    display: flex;
+    gap: 10px;
+    margin: 0;
+    list-style: none;
+    padding: 0;
+  }
 
+  .product {
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 15px;
+    width: 100%;
+    max-width: 800px;
+  }
+
+  .product-main {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+
+  .image-container {
+    width: 200px;
+    max-width: 400px;
+    overflow: hidden;
+  }
+
+  .image-container img {
+    width: 100%;
+  }
+
+  .slider {
+    position: relative;
+    width: 80%;
+    margin: 0 10%;
+  }
+
+  .slider-item {
+    width: 100%;
+  }
+
+  .slider-item.left {
+    position: absolute;
+    top: 0;
+    right: 100%;
+  }
+
+  .slider-item.right {
+    position: absolute;
+    top: 0;
+    left: 100%;
+  }
+
+  .slider-left-button {
+    position: absolute;
+    top: 50%;
+    left: -30px;
+    transform: translateY(-50%);
+    cursor: pointer;
+    z-index: 10;
+  }
+
+  .slider-right-button {
+    position: absolute;
+    top: 50%;
+    right: -30px;
+    transform: translateY(-50%);
+    cursor: pointer;
+    z-index: 10;
+  }
 </style>
+
+<header class="header">
+  <a class="header-title" href="/">Svelte EC</a>
+  <nav>
+    <ul class="header-links">
+      <li>ようこそゲストさん</li>
+      <li>
+        <a href="/cart">カート (0)</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<article class="product">
+  <div class="product-main">
+    <div class="image-container">
+      <div class="slider">
+        <img
+          src={product.images[sliderLeftIndex]}
+          alt="左側のスライダー"
+          class="slider-item left"
+        />
+        <img
+          src={product.images[sliderCenterIndex]}
+          alt="中央のスライダー"
+          class="slider-item center"
+        />
+        <img
+          src={product.images[sliderRightIndex]}
+          alt="右側のスライダー"
+          class="slider-item right"
+        />
+        <button on:click={sliderMoveLeft} class="slider-left-button">←</button>
+        <button on:click={sliderMoveRight} class="slider-right-button">→</button>
+      </div>
+    </div>
+
+    <div>
+      <h2>{product.name}</h2>
+      <dl>
+        <dt>価格</dt>
+        <dd>{product.price}</dd>
+      </dl>
+      <div>
+        {#if !cart.includes('svelte-book')}
+          <button on:click={() => addToCart('svelte-book')}>カートに入れる</button>
+        {:else}
+          <button disabled>カート追加済み</button>
+        {/if}
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <h3>関連商品</h3>
+    {#each relatedProducts as product}
+      <li>
+        <a href="/products/{product.id}">{product.name}</a>
+        - {product.price}円
+        <!-- {#if !cart.includes(product.id)}
+          <button on:click={() => addToCart(product.id)}>カートに入れる</button>
+        {:else}
+          <button disabled>カート追加済み</button>
+        {/if} -->
+      </li>
+    {/each}
+  </footer>
+</article>
